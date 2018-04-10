ce13d59c9512f19b9b24f9916b7b4a17eb2d2dff
Toby Smith <toby@tismith.id.au>
2018-04-10 15:30:25 +1000

    First two challenges

--- src/main.rs
+++ src/main.rs
@@ -36,10 +36,19 @@ fn run(_config: &Settings) -> Result<()> {
     trace!("Entry to top level run()");
     //DO STUFF
 
-    println!("Challenge 1");
+    println!("Set 1 Challenge 1");
     let buffer = hex::decode("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")?;
     let base64 = base64::encode(&buffer);
     println!("{}", base64);
+
+    println!("Set 1 Challenge 2");
+    let buffer1 = hex::decode("1c0111001f010100061a024b53535009181c")?;
+    let buffer2 = hex::decode("686974207468652062756c6c277320657965")?;
+    let xored : Vec<u8> = buffer1.iter().zip(buffer2.iter()).map(|(x,y)| x ^ y).collect();
+    println!("{}", hex::encode(&xored));
+
+    println!("Set 1 Challenge 3");
+    //...
 
     Ok(())
 }
