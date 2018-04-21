3b186762c9a678652b870cf7ddac07019ece685f
Toby Smith <toby@tismith.id.au>
2018-04-21 14:05:36 +1000

    Moving some files into common

--- src/common.rs
+++ src/common.rs
@@ -6,6 +6,33 @@ use bit_vec;
 #[cfg(test)]
 use hex;
 
+pub fn strip_pkcs7_padding(plaintext: &[u8]) -> utils::types::Result<Vec<u8>> {
+    let blocksize = 16;
+    if plaintext.len() % blocksize != 0 {
+        bail!("Invalid padding - not padded");
+    }
+    let last_chunk = plaintext.chunks(blocksize).last().unwrap();
+    let padding_char = *last_chunk.iter().last().unwrap();
+    if padding_char > blocksize as u8 {
+        //this isn't a pkcs#7 padding character
+        return Ok(plaintext.to_vec());
+    }
+    let mut expected_padding = padding_char;
+    for ch in last_chunk.iter().rev() {
+        if *ch == padding_char {
+            if expected_padding <= 0 {
+                bail!("Invalid padding - too many padding");
+            }
+            expected_padding -= 1;
+        } else if expected_padding == 0 {
+            return Ok(plaintext[..(plaintext.len() - padding_char as usize)].to_vec());
+        } else {
+            bail!("Invalid padding - incorrect padding");
+        }
+    }
+    bail!("Invalid padding");
+}
+
 pub fn read_base64_file(path: &str) -> utils::types::Result<Vec<u8>> {
     use std::io::Read;
     let mut file = std::fs::File::open(path).chain_err(|| format!("Failed to open {}", path))?;
@@ -179,9 +206,8 @@ pub fn aes_128_cbc_decrypt(
         block = xor(&iv, &block);
         iv = chunk.to_vec();
         cleartext.extend_from_slice(&block);
-        //should pkcs#7 unpad?
     }
-    Ok(cleartext)
+    Ok(strip_pkcs7_padding(&cleartext)?)
 }
 
 pub fn aes_128_cbc_encrypt(
@@ -241,5 +267,26 @@ mod test {
         let plaintext = b"YELLOW SUBMARINE";
         let known_good = b"YELLOW SUBMARINE\x04\x04\x04\x04";
         assert_eq!(pkcs7_pad(plaintext, 20), known_good);
+    }
+
+    #[test]
+    fn test_strip_pkcs7_padding () {
+        let test1 = b"0123456789\x06\x06\x06\x06\x06\x06";
+        let stripped = strip_pkcs7_padding(test1).unwrap();
+        assert_eq!(stripped, b"0123456789");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_strip_pkcs7_padding_error_incrementing () {
+        let test1 = b"0123456789\x01\x02\x03\x04\x05\x06";
+        let _ = strip_pkcs7_padding(test1).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_strip_pkcs7_padding_error_incorrect () {
+        let test1 = b"0123456789\x01\x01\x01\x01\x01\x01";
+        let _ = strip_pkcs7_padding(test1).unwrap();
     }
 }
--- src/set2.rs
+++ src/set2.rs
@@ -283,7 +283,7 @@ pub fn run_set2() -> utils::types::Result<()> {
 
     {
         println!("Set 2 Challenge 15");
-        println!("{}", std::str::from_utf8(&strip_pkcs7_padding(b"ICE ICE BABY\x04\x04\x04\x04")?)?);
+        println!("{}", std::str::from_utf8(&common::strip_pkcs7_padding(b"ICE ICE BABY\x04\x04\x04\x04")?)?);
 
     }
 
@@ -366,8 +366,7 @@ fn encryption_oracle(cleartext: &[u8]) -> utils::types::Result<(Vec<u8>, EcbOrCb
     } else {
         //CBC
         trace!("Chose CBC");
-        let mut iv = vec![0u8; 16];
-        rng.fill_bytes(&mut iv);
+        let iv : [u8; 16] = rand::random();
         Ok((
             common::aes_128_cbc_encrypt(&plaintext, &iv, &key)?,
             EcbOrCbc::CBC,
@@ -421,57 +420,9 @@ fn detect_ecb_or_cbc(cryptotext: &[u8]) -> EcbOrCbc {
     }
 }
 
-fn strip_pkcs7_padding(plaintext: &[u8]) -> utils::types::Result<Vec<u8>> {
-    let blocksize = 16;
-    if plaintext.len() % blocksize != 0 {
-        bail!("Invalid padding - not padded");
-    }
-    let last_chunk = plaintext.chunks(blocksize).last().unwrap();
-    let padding_char = *last_chunk.iter().last().unwrap();
-    if padding_char > blocksize as u8 {
-        //this isn't a pkcs#7 padding character
-        return Ok(plaintext.to_vec());
-    }
-    let mut expected_padding = padding_char;
-    for ch in last_chunk.iter().rev() {
-        if *ch == padding_char {
-            if expected_padding <= 0 {
-                bail!("Invalid padding - too many padding");
-            }
-            expected_padding -= 1;
-        } else if expected_padding == 0 {
-            return Ok(plaintext[..(plaintext.len() - padding_char as usize)].to_vec());
-        } else {
-            bail!("Invalid padding - incorrect padding");
-        }
-    }
-    bail!("Invalid padding");
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn test_strip_pkcs7_padding () {
-        let test1 = b"0123456789\x06\x06\x06\x06\x06\x06";
-        let stripped = strip_pkcs7_padding(test1).unwrap();
-        assert_eq!(stripped, b"0123456789");
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_strip_pkcs7_padding_error_incrementing () {
-        let test1 = b"0123456789\x01\x02\x03\x04\x05\x06";
-        let _ = strip_pkcs7_padding(test1).unwrap();
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_strip_pkcs7_padding_error_incorrect () {
-        let test1 = b"0123456789\x01\x01\x01\x01\x01\x01";
-        let _ = strip_pkcs7_padding(test1).unwrap();
-    }
 
     #[test]
     fn test_detect_ecb_or_cbc() {
